128833e926d59bc2d42a5ea15a3a416986c15fb7
owain lewis <owain@owainlewis.com>
2014-01-14 10:49:17 +0000

    make repo urls dynamic

--- app.rb
+++ app.rb
@@ -2,11 +2,14 @@ require 'sinatra'
 require 'octokit'
 require 'rdiscount'
 
-REPOSITORY = 'scheduling'
 ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
 
 get '/' do
+  erb :index
+end
+
+get '/:repository' do
   client = Octokit::Client.new access_token: ACCESS_TOKEN
-  releases = client.repo("boxuk/#{ REPOSITORY }").rels[:releases].get.data
-  erb :index, locals: { releases: releases }
+  releases = client.repo("boxuk/#{ params[:repository] }").rels[:releases].get.data
+  erb :repository, locals: { releases: releases }
 end
